ea6599b1527f192917c57f259de7f8460832e046
Eduardo Carmo <eduardocarmo72@gmail.com>
2025-02-17 23:39:53 -0300

    aula de comunicação direta finalizada

---
+++ src/components/comunicacao/GerarNumero.tsx
@@ -0,0 +1,21 @@
+import Area from "../template/Area";
+
+interface GerarNumeroProps {
+    numero1: number
+    numero2: number
+}
+
+export default function GerarNumero(props: GerarNumeroProps) {
+
+    const min = Math.min(props.numero1, props.numero2)
+    const max = Math.max(props.numero1, props.numero2)
+
+    return (
+        <Area titulo="Gerar Número" cor="blue">
+            <div className="flex justify-center gap-5 text-2xl">
+                <span>Min: {min}</span>
+                <span>Max: {max}</span>
+            </div>
+        </Area>
+    )
+}
