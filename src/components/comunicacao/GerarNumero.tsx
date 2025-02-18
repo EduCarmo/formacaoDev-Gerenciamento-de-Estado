eddf455a67c57b58b2e32ba5c8461461b288abfb
Eduardo Carmo <eduardocarmo72@gmail.com>
2025-02-18 19:32:06 -0300

    aula de revisao finalizada

--- src/components/comunicacao/GerarNumero.tsx
+++ src/components/comunicacao/GerarNumero.tsx
@@ -3,6 +3,7 @@ import Area from "../template/Area";
 interface GerarNumeroProps {
     numero1: number
     numero2: number
+    gerarNumero: (resultado: number) => void
 }
 
 export default function GerarNumero(props: GerarNumeroProps) {
@@ -10,11 +11,19 @@ export default function GerarNumero(props: GerarNumeroProps) {
     const min = Math.min(props.numero1, props.numero2)
     const max = Math.max(props.numero1, props.numero2)
 
+    function gerar() {
+        const aleatorio = Math.round(Math.random() * (max - min) + min)
+        props.gerarNumero?.(aleatorio)
+    }
+
     return (
         <Area titulo="Gerar Número" cor="blue">
-            <div className="flex justify-center gap-5 text-2xl">
-                <span>Min: {min}</span>
-                <span>Max: {max}</span>
+            <div className="flex flex-col items-center gap-5">
+                <div className="flex justify-center gap-5 text-2xl">
+                    <span>Min: {min}</span>
+                    <span>Max: {max}</span>
+                </div>
+                <button onClick={gerar} className="btn">Gerar</button>
             </div>
         </Area>
     )
--- src/components/comunicacao/Mostrador.tsx
+++ src/components/comunicacao/Mostrador.tsx
@@ -6,9 +6,17 @@ export default function Mostrador() {
 
     const [n1, setN1] = useState(0)
     const [n2, setN2] = useState(0)
+    const [resultado, setResultado] = useState(0)
+
+    function exibirResultado(numero: number) {
+        setResultado(numero)
+    }
 
     return (
         <Area titulo="Mostrador" cor="red">
+            <div className="flex justify-center font-black text-6xl">
+                {resultado}
+            </div>
             <div className="flex gap-5 p-5">
                 <input
                     type="number"
@@ -26,6 +34,7 @@ export default function Mostrador() {
             <GerarNumero 
                 numero1={n1}
                 numero2={n2}
+                gerarNumero={exibirResultado}
             />
         </Area>
     )
